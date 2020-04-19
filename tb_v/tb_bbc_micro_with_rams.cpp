7242da3842106db7969dca728a3bfcbb26b1f59b
Gavin Stark <gjstark@me.com>
2020-04-19 14:39:52 +0100

    [verilator] Added initial verilator testbench

---
+++ tb_v/tb_bbc_micro_with_rams.cpp
@@ -0,0 +1,45 @@
+// make CDL_EXTRA_FLAGS='--v_clks_must_have_enables  --v_use_always_at_star' clean_verilog verilog
+// verilator --cc -f a --top-module bbc_micro_with_rams -Wno-fatal
+// export VERILATOR_ROOT=~/Git/cdl_tools_grip/tools/share/verilator/
+// make -f Vbbc_micro_with_rams.mk 
+// g++ ../../atcf_hardware_bbc/tb_v/tb_bbc_micro_with_rams.cpp ${VERILATOR_ROOT}/include/verilated.cpp Vbbc_micro_with_rams__ALL.a -I${VERILATOR_ROOT}/include -I.
+
+#include <verilated.h>
+#include "Vbbc_micro_with_rams.h"
+double sc_time_stamp () {
+    return 0;
+}
+int main(int argc, char** argv, char** env) {
+    (void) argc;
+    (void) argv;
+    (void) env;
+    long long clks;
+
+    Vbbc_micro_with_rams* m = new Vbbc_micro_with_rams;
+    m->reset_n = 0;
+    m->clk = 0;
+    m->video_clk = 0;
+    clks = 0;
+    while (!Verilated::gotFinish() && (clks<100*1000*1000)) {
+        int toggled=0;
+        clks++;
+        if ((clks%4)==0) {
+            m->clk = !m->clk;
+            toggled = 1;
+        }
+        if ((clks%4)==0) {
+            m->video_clk = !m->video_clk;
+            toggled = 1;
+        }
+        if ((!m->clk) && (clks>10)) {
+            m->reset_n = 1;
+            toggled = 1;
+        }
+        if (toggled) {
+            m->eval();
+        }
+    }
+    m->final();
+    delete m;
+    return 0;
+}
